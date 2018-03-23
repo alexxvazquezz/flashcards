52d0557fc27554072baa8646b2b68babeb368799
Alejandro Vazquez <alexv@alejandros-mbp.home>
2018-03-23 07:22:44 -0700

    test

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello whatup DOG I love you');
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Hello whatup DOG I love you</h1>');
+})
 
-app.listen(3000);
+app.get('/hello', (req, res) => {
+  res.send('<h2>Hello motherfucker</h2>')
+})
+
+app.listen(3000, () => {
+  console.log('The application is runing on localhost:3000');
+})
