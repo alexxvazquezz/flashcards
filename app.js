571fbfc7da2619710de3beee97a60ce1e9c23bf6
Alejandro Vazquez <avazquez@alejandros-mbp.home>
2018-04-23 20:27:52 -0700

    new files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -28,13 +29,34 @@ app.get('/usertable', (req, res) => {
 })
 
 app.get('/hello', (req, res) => {
-  res.render('hello', { name: req.cookies.username });
+  const name = req.cookies.username
+  if(name) res.redirect('/');
+  else res.render('hello', { name });
 })
 
 app.post('/hello', (req, res) => {
   res.cookie('username', req.body.username)
   res.redirect('/');
 })
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+})
+
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error')
+})
+
 
 
 app.listen(3000, () => {
